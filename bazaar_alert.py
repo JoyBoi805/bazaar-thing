1327a06b8b68ec62a8c9144a5884e1779bd9eaf7
JoyBoi805 <147010045+JoyBoi805@users.noreply.github.com>
2026-03-28 10:42:08 -0700

    Added booster cookie to the list of trackable items

--- bazaar_alert.py
+++ bazaar_alert.py
@@ -3,12 +3,16 @@ import time
 
 apiKey  = "e71f4128-0eeb-4e7a-bec7-f00be861e1a8"
 webhook  = "https://discord.com/api/webhooks/1487307771236978708/Aj7r4gSjrMEmwPJE6_oESoiVeYeuVgtgV5QKK7YRnKNI5VvvBu9hShwp8QCfnWmTiPmj"
-secondsBetween    = 120
+secondsBetween = 5
 minVolume = 1000
 ALERTS = {
     "VERY_CRUDE_GABAGOOL": {
         "above": 50_000,
         # "below": 150_000,
+    },
+    "BOOSTER_COOKIE": {
+        #"above": 50_000,
+        "below": 9_500_000,
     },
 }
 
